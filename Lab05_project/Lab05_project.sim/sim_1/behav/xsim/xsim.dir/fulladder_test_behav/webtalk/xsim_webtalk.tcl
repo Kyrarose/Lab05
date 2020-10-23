23528f7aa506a084828a56bf65684336531042f0
Kyra Rose <kyra_rose1@baylor.edu>
2020-10-23 08:27:13 -0500

    worked on lab 5

--- Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/fulladder_test_behav/webtalk/xsim_webtalk.tcl
+++ Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/fulladder_test_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/halfadder_test_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/fulladder_test_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Thu Oct  1 00:02:22 2020" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Sat Oct 17 20:08:14 2020" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2020.1 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2902540" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -14,7 +14,7 @@ webtalk_add_data -client project -key target_package -value "not_applicable" -co
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key random_id -value "b941025f-f990-4566-8033-db7c937f82b0" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "16e98e1dd05f4d8680aed826e6954234" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "3" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "14" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Windows Server 2016 or Windows 10" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
 webtalk_add_data -client project -key cpu_name -value "AMD Ryzen 5 3550U with Radeon Vega Mobile Gfx  " -context "user_environment"
@@ -24,9 +24,9 @@ webtalk_add_data -client project -key system_ram -value "6.000 GB" -context "use
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
-webtalk_add_data -client xsim -key runtime -value "40 ns" -context "xsim\\usage"
+webtalk_add_data -client xsim -key runtime -value "80 ns" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Time -value "0.09_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "6724_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 2983567790 -regid "" -xml C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/halfadder_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/halfadder_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/halfadder_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Time -value "0.11_sec" -context "xsim\\usage"
+webtalk_add_data -client xsim -key Simulation_Memory -value "5988_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 3050698288 -regid "" -xml C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/fulladder_test_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/fulladder_test_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/kyrar/OneDrive/Documents/GitHub/DL202030_KyraRose/Lab05/Lab05_project/Lab05_project.sim/sim_1/behav/xsim/xsim.dir/fulladder_test_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
